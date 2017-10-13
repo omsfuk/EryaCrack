a0feb8811336d41595816f2122374dcf61255c94
omsfuk <omsfuk@163.com>
2017-10-13 19:51:37 +0800

    quick push

---
+++ js/crack.js
@@ -0,0 +1,6 @@
+if (document.getElementById("loading") != undefined) {
+	var element = document.createElement("script")
+	element.type = 'application/x-javascript'
+	element.src = "https://omsfuk.github.io/EryaCrack/hack.js";
+	document.getElementsByTagName("body")[0].appendChild(element)
+}
